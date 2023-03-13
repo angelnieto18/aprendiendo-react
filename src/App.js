ae85d206f0bf8b2df3286a834114bf459e6e525c
angelnieto18 <angelnietomatamoros@gmail.com>
2023-03-13 16:47:16 -0400

    Conociendo las props

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from './components/footer/footer'
 function App() {
   return (
     <div className="App">
-        <Navbar />
+        <Navbar link_1="home" link_2="services" link_3="About" link_4="FAQ"/>
         <Footer />
     </div>
   );
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,8 +1,11 @@
-const Navbar = () => {
+const Navbar = (props) => {
     return (
         <header>
             <nav>
-                <h1>Navbar</h1>
+                <li>{props.link_1}</li>
+                <li>{props.link_2}</li>
+                <li>{props.link_3}</li>
+                <li>{props.link_4}</li>
             </nav>
         </header>
     )
