6668aed61e8f9c5b6cce2207f14c1801462360f8
angelnieto18 <angelnietomatamoros@gmail.com>
2023-03-17 14:05:27 -0400

    preparando una barra de navegación en react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
-import Navbar from './components/navbar/Navbar'
-import Footer from './components/footer/Footer'
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-        <Navbar link_1="home" link_2="services" link_3="About" link_4="FAQ"/>
-        <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar
+                link_1="home"
+                link_2="services"
+                link_3="About"
+                link_4="FAQ"
+            />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,16 +1,24 @@
-import './navbar.css'
+import "./navbar.css";
 
 const Navbar = (props) => {
     return (
-        <header>
-            <nav>
-                <li>{props.link_1}</li>
-                <li>{props.link_2}</li>
-                <li>{props.link_3}</li>
-                <li>{props.link_4}</li>
+        <header className="header">
+            <nav className="navbar">
+                <div className="navbar__logo-container">
+                    <ion-icon name="logo-hackernews"></ion-icon>
+                </div>
+                <div className="navbar__menuBtn-container">
+                    <ion-icon name="menu-outline"></ion-icon>
+                </div>
+                <div className="navbar__menu-container">
+                    <li>{props.link_1}</li>
+                    <li>{props.link_2}</li>
+                    <li>{props.link_3}</li>
+                    <li>{props.link_4}</li>
+                </div>
             </nav>
         </header>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
