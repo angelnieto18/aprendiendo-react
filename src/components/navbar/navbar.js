528ac9b70da0588e778afd7421a04686c16e9d6a
angelnieto18 <angelnietomatamoros@gmail.com>
2023-03-17 19:38:39 -0400

    el responsive funciona perfecto

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,13 +2,17 @@ import "./navbar.css";
 import { useState } from "react";
 
 const Navbar = (props) => {
+    const menu = document.querySelector(".navbar__menu-list");
+
     let [name, setName] = useState("menu-outline");
 
     const handleClick = (e) => {
         if (e.target.name === "menu-outline") {
             setName("close-outline");
+            menu.style.left = "0";
         } else {
             setName("menu-outline");
+            menu.style.left = "100%";
         }
     };
 
@@ -22,10 +26,12 @@ const Navbar = (props) => {
                     <ion-icon onClick={handleClick} name={name}></ion-icon>
                 </div>
                 <div className="navbar__menu-container">
-                    <li>{props.link_1}</li>
-                    <li>{props.link_2}</li>
-                    <li>{props.link_3}</li>
-                    <li>{props.link_4}</li>
+                    <ul className="navbar__menu-list">
+                        <li className="navbar__menu-listItem">{props.link_1}</li>
+                        <li className="navbar__menu-listItem">{props.link_2}</li>
+                        <li className="navbar__menu-listItem">{props.link_3}</li>
+                        <li className="navbar__menu-listItem">{props.link_4}</li>
+                    </ul>
                 </div>
             </nav>
         </header>
