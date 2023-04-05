863269d11c8612ae7a0c1464b64388eac0ad0c1f
angelnieto18 <angelnietomatamoros@gmail.com>
2023-04-05 16:08:27 -0400

    Errores en la barra de navegación corregidos

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,41 +1,44 @@
-import "./navbar.css";
-import { useState } from "react";
+import './navbar.css';
+import { useState } from 'react';
 
 const Navbar = (props) => {
-    const menu = document.querySelector(".navbar__menu-list");
+  let [name, setName] = useState('menu-outline');
 
-    let [name, setName] = useState("menu-outline");
+  const toggleAttribute = () => {
+    const checkBtn = document.querySelector('.checkBtn');
+    checkBtn.checked === false ? (checkBtn.checked = true) : (checkBtn.checked = false);
+  };
 
-    const handleClick = (e) => {
-        if (e.target.name === "menu-outline") {
-            setName("close-outline");
-            menu.style.left = "0";
-        } else {
-            setName("menu-outline");
-            menu.style.left = "100%";
-        }
-    };
-
-    return (
-        <header className="header">
-            <nav className="navbar">
-                <div className="navbar__logo-container">
-                    <ion-icon name="logo-hackernews"></ion-icon>
-                </div>
-                <div className="navbar__menuBtn-container">
-                    <ion-icon onClick={handleClick} name={name}></ion-icon>
-                </div>
-                <div className="navbar__menu-container">
-                    <ul className="navbar__menu-list">
-                        <li className="navbar__menu-listItem">{props.link_1}</li>
-                        <li className="navbar__menu-listItem">{props.link_2}</li>
-                        <li className="navbar__menu-listItem">{props.link_3}</li>
-                        <li className="navbar__menu-listItem">{props.link_4}</li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <div className="navbar__logo-container">
+          <ion-icon name="logo-hackernews"></ion-icon>
+        </div>
+        <input className="checkBtn" type="checkbox" />
+        <div className="navbar__menuBtn-container">
+          <ion-icon
+            onClick={() => {
+              name === 'menu-outline' ? setName('close-outline') : setName('menu-outline');
+              toggleAttribute();
+            }}
+            name={name}
+          ></ion-icon>
+        </div>
+        <div className="navbar__menu-container">
+          <ul className="navbar__menu-list">
+            <li className="navbar__menu-listItem">{props.link_1}</li>
+            <li className="navbar__menu-listItem">{props.link_2}</li>
+            <li className="navbar__menu-listItem">{props.link_3}</li>
+            <li className="navbar__menu-listItem">{props.link_4}</li>
+          </ul>
+        </div>
+      </nav>
+      <div className="header__container">
+        <h1 className="header__container-title">¡Hola!</h1>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
