df4c8268aed44021ba037e668abe3eba9ff1a7df
angelnieto18 <angelnietomatamoros@gmail.com>
2023-05-01 19:35:12 -0400

    Aprendiendo a hacer api call con react-query

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import './App.css';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </Router>
+    <div className='App'>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/profile' element={<Profile />} />
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
 import Header from '../components/Header/Header';
 import Footer from '../components/footer/Footer';
 
+const getUsers = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+  return data;
+};
+
 const Home = () => {
+  const { data } = useQuery({
+    queryKey: ['getUsers'],
+    queryFn: getUsers,
+  });
+
+  console.log(data);
+
   return (
     <>
       <Header />
